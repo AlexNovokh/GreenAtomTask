2219226338d90bb0a47be6f93bf78226fc3f9de0
AlexNovokh <164947008+AlexNovokh@users.noreply.github.com>
2024-03-26 21:35:44 +0300

    Update 1 main.py
    Добавлены функции запуска и остановки робота

--- main.py
+++ main.py
@@ -1,12 +1,34 @@
 from fastapi import FastAPI, Query
+import subprocess
 import uvicorn
+import psutil
 
 app = FastAPI()
 
 
 @app.get("/")
 async def root():
-    return {"message": f"You can start_robot or stop_robot"}
+    return {"message": f"You can start_robot, stop_robot or see_history"}
+
+
+# Запуск робота
+@app.get("/start_robot")
+async def start_robot(start_number: int = Query(0)):
+    subprocess.Popen(["python", "robot.py", str(start_number)], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    return {"message": f"Robot started from number {start_number}"}
+
+
+# Остановка робота
+@app.get("/stop_robot")
+async def stop_robot():
+    for proc in psutil.process_iter():
+        try:
+            cmdline = proc.cmdline()
+            if cmdline and cmdline[0] == "python" and "robot.py" in cmdline[1:]:
+                proc.kill()
+        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+            pass
+    return {"message": f"Robot was stopped"}
 
 
 # Запуск веб-сервера
