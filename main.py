7b2997cc08730620452270ba15a3c46365c57b3f
AlexNovokh <164947008+AlexNovokh@users.noreply.github.com>
2024-03-27 16:01:42 +0300

    Update 5 main.py
    Исправлено введение данных в базу данных

--- main.py
+++ main.py
@@ -35,8 +35,8 @@ async def start_robot(start_number: int = Query(0)):
     if has_started():
         return {"message": f"Robot has already started from number {start_number}"}
     else:
-        arr[0] = f"{start_number}"
-        arr[1] = f"{datetime.datetime.now()}"
+        arr[0] = start_number
+        arr[1] = datetime.datetime.now()
         subprocess.Popen(["python", filename, str(start_number)], stderr=subprocess.PIPE)
         return {"message": f"Robot started from number {start_number}"}
 
@@ -47,7 +47,8 @@ async def stop_robot():
     proc = has_started()
     if proc:
         proc.kill()
-        arr[2] = f"{datetime.datetime.now()}"
+        a = datetime.datetime.now() - arr[1]
+        arr[2] = a.seconds
         work_with_db()
         return {"message": f"Robot was stopped"}
     else:
@@ -56,7 +57,7 @@ async def stop_robot():
 
 # Внесение данных в базу данных
 def work_with_db():
-    con = sqlite3.connect("new1.db")
+    con = sqlite3.connect("robot_hystory.db")
     cur = con.cursor()
     cur.execute("CREATE TABLE IF NOT EXISTS rb_history(start_number TEXT, "
                 "start_time TEXT, total_time TEXT)")
