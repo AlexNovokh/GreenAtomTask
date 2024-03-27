564bc7a4469a0fc79d0b21647a75f6321c93bbef
AlexNovokh <164947008+AlexNovokh@users.noreply.github.com>
2024-03-27 13:44:41 +0300

    Update 3 main.py
    Добавлена функция для проверки состояния робота

--- main.py
+++ main.py
@@ -6,33 +6,43 @@ import psutil
 app = FastAPI()
 
 # Название файла, содержащего код робота
-filename = "robot.py"  
+filename = "robot.py"
 
+# Проверка состояния робота
+def has_started():
+    for proc in psutil.process_iter():
+        try:
+            cmdline = proc.cmdline()
+            # Поиск команды запуска файла
+            if cmdline and cmdline[0] == "python" and filename in cmdline[1:]:
+                return proc
+        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+            pass
 
 @app.get("/")
 async def root():
-    return {"message": f"You can start_robot, stop_robot or see_history"}
+    return {"message": f"You can start_robot, stop_robot"}
 
 
 # Запуск робота
 @app.get("/start_robot")
 async def start_robot(start_number: int = Query(0)):
-    subprocess.Popen(["python", filename, str(start_number)], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    return {"message": f"Robot started from number {start_number}"}
+    if has_started():
+        return {"message": f"Robot has already started from number {start_number}"}
+    else:
+        subprocess.Popen(["python", filename, str(start_number)], stderr=subprocess.PIPE)
+        return {"message": f"Robot started from number {start_number}"}
 
 
 # Остановка робота
 @app.get("/stop_robot")
 async def stop_robot():
-    for proc in psutil.process_iter():
-        try:
-            cmdline = proc.cmdline()
-            # Поиск команды запуска файла
-            if cmdline and cmdline[0] == "python" and filename in cmdline[1:]:  
-                proc.kill()
-        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
-            pass
-    return {"message": f"Robot was stopped"}
+    proc = has_started()
+    if proc:
+        proc.kill()
+        return {"message": f"Robot was stopped"}
+    else:
+        return {"message": f"Robot wasn't working"}
 
 
 # Запуск веб-сервера
