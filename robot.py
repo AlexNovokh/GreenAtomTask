be26dc2a8d9e407a7b065d481babe26c40e47c7c
AlexNovokh <164947008+AlexNovokh@users.noreply.github.com>
2024-03-28 09:20:12 +0300

    Финальная версия

--- robot.py
+++ robot.py
@@ -2,7 +2,7 @@ import asyncio
 import sys
 
 
-#Прибавление 1 каждую секунду
+# Прибавление 1 каждую секунду
 async def robot(start_number=0):
     count = start_number
     while True:
@@ -14,7 +14,7 @@ async def robot(start_number=0):
 async def main():
     try:
         start_number = int(sys.argv[1]) if len(sys.argv) == 2 else 0
-    except (ValueError): #Если введено не число
+    except ValueError:  # Если введено не число
         start_number = 0
     task = asyncio.create_task(robot(start_number))
     await task
